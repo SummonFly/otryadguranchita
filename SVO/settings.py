4d0363b31d1fb4d0274e2710327edb760261f7e4
SummonFly <109837346+SummonFly@users.noreply.github.com>
2026-01-25 19:31:40 +0900

    Test

--- SVO/settings.py
+++ SVO/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -62,14 +63,15 @@ DATABASES = {
 }
 
 # Static files
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-# WhiteNoise configuration
+# Включение поддержки сжатия и кэширования
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Другие настройки...
 LANGUAGE_CODE = 'en-us'
