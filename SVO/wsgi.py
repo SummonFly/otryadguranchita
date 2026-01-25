9025238fe70dc81f95534debac3738f89e844b36
SummonFly <109837346+SummonFly@users.noreply.github.com>
2026-01-25 20:25:54 +0900

    TryWhiteNoise

--- SVO/wsgi.py
+++ SVO/wsgi.py
@@ -10,8 +10,13 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SVO.settings')
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 application = get_wsgi_application()
+application = WhiteNoise(application, root=STATIC_ROOT)
 app = application
